5b4f82bfa2e04dd3bf1e695aa32daeb97aba9572
takushikai <takushikai2@gmail.com>
2021-10-04 00:33:07 +0900

    ok

--- js/startmenu.js
+++ js/startmenu.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function $get(str){
     return document.querySelector(str);
 }
@@ -27,34 +26,4 @@ function start(){
     }
 
     //メディアがあるならここで再生させるべき
-=======
-function $get(str){
-    return document.querySelector(str);
-}
-
-$get("#startBtn").onclick = function(){
-    start();
-}
-
-
-$get("#roomCode").onkeydown = function(e){
-    if(e.key == "Enter"){
-        e.preventDefault();
-        start();
-    }
-}
-
-
-function start(){
-    roomCode = $get("#roomCode").value;
-    startmenu.hidden = true;
-    pageframe.hidden = false;
-    const slideHTML = window.open("./slide.html");
-    slideHTML.onload = function(){
-        $get("#slideRapper").insertAdjacentHTML("afterbegin",slideHTML.document.body.innerHTML);
-        slideHTML.close();
-    }
-
-    //メディアがあるならここで再生させるべき
->>>>>>> d9bac84c94039437db23939bf2dc6cf7a1181e46
 }
