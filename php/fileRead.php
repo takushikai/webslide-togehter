d18447b259d5b916e923e4ae60ffb8a8e3daa6f7
takushikai <takushikai2@gmail.com>
2021-10-04 00:48:23 +0900

    ok

--- php/fileRead.php
+++ php/fileRead.php
@@ -2,7 +2,7 @@
     $roomCode = $_POST["message"];
     // file_put_contents("./pageCount.txt",$roomCode);
 
-    $file = "./data.json";
+    $file = "../data.json";
     $json = file_get_contents($file); //指定したファイルの要素をすべて取得する
     $arr = json_decode($json,true);
     $page = $arr[$roomCode];
--- php/fileWrite.php
+++ php/fileWrite.php
@@ -5,7 +5,7 @@
     $page = $input_data["page"];
     // file_put_contents("./pageCount.txt",$roomCode);
 
-    $file = "./data.json";
+    $file = "../data.json";
     $json = file_get_contents($file); //指定したファイルの要素をすべて取得する
     $arr = json_decode($json,true);
     $arr[$roomCode] = $page;
